ab28af0047662db7b52511a84d58e970e60bdffb
Tobias Roth <tobias@Mac-Tobi-2.local>
2020-04-27 19:37:12 +0200

    Worked example to calculate Spear-Index
    - R-Skript with worked example
    - Two taxa files to get Spear-Class for each taxa
    - Remove earlier example that was used to compare with indicate.
    - ReadMe

--- R/spearR.R
+++ R/spearR.R
@@ -2,40 +2,68 @@
 rm(list=ls(all=TRUE))
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Settings and load data ----
+# Settings ----
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Libraries
 library(tidyverse)
+library(readxl)
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Read and prepare species data (export from Midat) ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Load data
-dat <- read_csv("Data/monitoring_data.csv")
+d <- read_excel("Data/Beispielsexport-MIDAT.xlsx") %>% 
+  transmute(
+    siteID = `Numéro station`,
+    Datum = as.Date(paste(Année, Mois, Jour, sep = "-")) %>% as.character(),
+    Phylum = Phylum,
+    Klasse = Classe,
+    Ordnung = Ordre,
+    Familie = Famille,
+    Ind = as.integer(Abondance)
+  )
+
+# Construct IBCH-Taxa 
+d$Taxa_IBCH <- apply(d[, c("Phylum", "Klasse", "Ordnung", "Familie")], 1, function(x) x[last(which(!is.na(x)))])
+d$Taxa_IBCH[d$Taxa_IBCH == "Psephenidae"] <- "Psephenidae (=Eubriidae)"
+d$Taxa_IBCH[d$Taxa_IBCH == "Scirtidae"] <- "Scirtidae (=Helodidae)"
+d$Taxa_IBCH[d$Taxa_IBCH == "Nematoda"] <- "Nemathelminthes"
+d$Taxa_IBCH[d$Taxa_IBCH == "Acari"] <- "Hydracarina"
+d$Taxa_IBCH[d$Taxa_IBCH == "Arachnida"] <- "Hydracarina"
+d$Taxa_IBCH[d$Taxa_IBCH == "Prostigmata"] <- "Hydracarina"
+d <- d %>% filter(Taxa_IBCH != "Diptera")
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Taxa traits and classification ----
+# Add spear class to data ----
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-dat %>% 
-  left_join(read_csv("Indicate_tables/Trat_Database.csv")) %>% 
-  mutate(SPEAR_class = as.integer(Sensitivity > -0.36 & Generation >= 0.5 & Refuge == 1 & Exposed == 1)) %>% 
-  group_by(Taxa) %>% 
-  summarise(
-    Sensitivity = mean(Sensitivity),
-    Generation = mean(Generation),
-    Refuge = mean(Refuge),
-    Exposed = mean(Exposed),
-    SPEAR_class = mean(SPEAR_class)) %>% 
-  data.frame()
+# Load taxa list with Midat (DM) and spear taxa names and the traits to
+# calculate the Spear class
+li <- left_join(
+  read_xlsx("Taxa_lists/1650_A32_Artangaben_EPT_v4.xlsx"),
+  read_xlsx("Taxa_lists/1650_Inidicate_trait_database.xlsx"),
+  by = c("Taxa_Spear_Calculator" = "Taxa")) %>% 
+  mutate(SPEAR_class = as.integer(Sensitivity > -0.36 & Generation >= 0.5 & Refuge == 1 & Exposed == 1)
+)
+
+# Add SPEAR class to data
+d <- 
+  d %>% 
+  left_join(
+    li %>% 
+      dplyr::select(Taxaname_BDM, SPEAR_class),
+    by = c("Taxa_IBCH" = "Taxaname_BDM"))
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Calculate SPEAR-Index ----
+# Calculate SPEAR  ----
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-dat %>%
-  left_join(read_csv("Indicate_tables/Trat_Database.csv")) %>%
-  mutate(SPEAR_class = as.integer(Sensitivity > -0.36 &
-                                    Generation >= 0.5 &
-                                    Refuge == 1 &
-                                    Exposed == 1)) %>%
-  group_by(Name_1, Name_2) %>%
-  summarise(SPEAR = round(100 * sum(log10(4 * Abundance + 1) * SPEAR_class) /
-                            sum(log10(4 * Abundance + 1))  / 34, 2))
 
+# Aggregate on IBCH-taxa level and calcualte Spear-Index
+d %>% 
+  filter(!is.na(SPEAR_class)) %>%
+  group_by(siteID, Datum, Taxa_IBCH, SPEAR_class) %>%
+  summarise(Ind = sum(Ind)) %>%
+  group_by(siteID, Datum) %>%
+  dplyr::summarise(
+    SPEAR = round(100 * sum(log10(4 * Ind + 1) * SPEAR_class) / sum(log10(4 * Ind + 1)), 2)) %>% 
+  as.data.frame()
 
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# spearR
-Compare Spear[Pesticide]-Index calculated with own r functions with results from `Indicate`.
+# spearR: calculate Spear-Index from Midat-data
+R function to calculate Spear-Index from Midat-data including a worked example.
+
+## Content
+
+**Data**
+
+Contains a sample export from Midat. They include two surveys from the same site.
+
+**R**
+
+Contains an R-Skripts that reads the raw data (from Midat) and calculates the Spear-Index for the two surveys. 
+
+**Taxa_lists**
+
+- `1650_A32_Artangaben_EPT_v4.xlsx`: Taxa list that is used to link the IBCH taxa names to the respective taxa from indicate.
+- `1650_Inidicate_trait_database.xlsx`: Taxa list exported from indicate including trait information that is used to calculate the spear index.
+
+## Version-History
+
+- This [versions](https://github.com/TobiasRoth/spearR/tree/c6a33ca3f3d00fd316664eb99859eb96273fd9d7) contains different scripts to compare the Spear-Index calculated with our own r functions with results from `Indicate` (version 2018). 
+
